440e76d3bcc8f9fbb4ef946db3151e60fe612d5d
himidori <himidori1@yandex.ru>
2018-07-11 02:42:17 +0300

    Removed unused method

--- utils/utils.go
+++ utils/utils.go
@@ -1,73 +1,68 @@
 package utils
 
 import (
-    "bufio"
-    "fmt"
-    "os"
-    "os/exec"
-    "runtime"
-    "strings"
+	"bufio"
+	"fmt"
+	"os"
+	"os/exec"
+	"runtime"
+	"strings"
 )
 
 func PathExists(path string) bool {
-    if _, err := os.Stat(path); err != nil {
-        if os.IsNotExist(err) {
-            return false
-        }
-    }
-    return true
+	if _, err := os.Stat(path); err != nil {
+		if os.IsNotExist(err) {
+			return false
+		}
+	}
+	return true
 }
 
 func Mkdir(path string) error {
-    return os.MkdirAll(path, 0755)
+	return os.MkdirAll(path, 0755)
 }
 
 func Mkfile(name string) error {
-    _, err := os.Create(name)
-    return err
+	_, err := os.Create(name)
+	return err
 }
 
 func Rmfile(path string) error {
-    return os.Remove(path)
+	return os.Remove(path)
 }
 
 func GetPrefix() string {
-    if runtime.GOOS == "darvin" {
-        return "/tmp/"
-    }
-    return "/dev/shm/"
-}
-
-func Cmd(name string, args ...string) error {
-    _, err := exec.Command(name, args...).Output()
-    return err
+	if runtime.GOOS == "darvin" {
+		return "/tmp/"
+	}
+	return "/dev/shm/"
 }
 
 func OpenURL(url string) {
-    if !strings.HasPrefix(url, "http://") {
-        url = "http://" + url
-    }
+	if !strings.HasPrefix(url, "http://") {
+		url = "http://" + url
+	}
 
-    var err error
-    switch runtime.GOOS {
-    case "linux":
-        err = exec.Command("xdg-open", url).Start()
+	var err error
+	switch runtime.GOOS {
+	case "linux":
+		err = exec.Command("xdg-open", url).Start()
 
-    case "darwin":
-        err = exec.Command("open", url).Start()
-    }
+	case "darwin":
+		err = exec.Command("open", url).Start()
+	}
 
-    if err != nil {
-        fmt.Printf("failed to open url %s: %s\n", url, err)
-    }
+	if err != nil {
+		fmt.Printf("failed to open url %s: %s\n", url, err)
+	}
 }
 
 func ReadLine() (string, error) {
-    reader := bufio.NewReader(os.Stdin)
-    line, err := reader.ReadString('\n')
-    if err != nil {
-        return "", err
-    }
+	reader := bufio.NewReader(os.Stdin)
+	line, err := reader.ReadString('\n')
+	if err != nil {
+		return "", err
+	}
 
-    return strings.TrimSpace(line), nil
+	return strings.TrimSpace(line), nil
 }
